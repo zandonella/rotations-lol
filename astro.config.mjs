20133d66d4008aaa43194189a9e59be3ee5c6d4f
zandonella <123271994+zandonella@users.noreply.github.com>
2025-12-15 22:19:27 -0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    devToolbar: {
+        enabled: false,
+    },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [react()],
+});
---
+++ src/components/authButton.jsx
@@ -0,0 +1,10 @@
+export default function AuthButton() {
+    return (
+        <button
+            type="button"
+            className="cursor-pointer rounded bg-amber-400 px-4 py-2 font-semibold text-gray-950 transition hover:bg-yellow-400"
+        >
+            Sign in
+        </button>
+    );
+}
