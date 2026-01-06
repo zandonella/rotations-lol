ad87a7e544ade416c5b68c7c79d79688b810aa06
zandonella <123271994+zandonella@users.noreply.github.com>
2026-01-06 01:36:21 -0800

    add theme toggle back

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,18 +1,31 @@
-// import { useTheme } from '../ThemeProvider';
 import { LuSun, LuMoon } from 'react-icons/lu';
+import type { Theme } from '../types';
+import { useEffect, useState } from 'react';
 
 export default function ThemeButton() {
-    // const { toggle, theme } = useTheme();
+    const [theme, setTheme] = useState<Theme>('dark');
+
+    useEffect(() => {
+        const stored = localStorage.getItem('theme') as Theme | null;
+        if (stored) {
+            setTheme(stored);
+        }
+    }, []);
+
     function toggle() {
-        console.log('toggle theme');
+        const newTheme: Theme = theme === 'dark' ? 'light' : 'dark';
+
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+        document.documentElement.classList.toggle('dark', newTheme === 'dark');
     }
+
     return (
         <button
             className="hover:text-accent cursor-pointer px-1 transition-colors duration-200 ease-in-out"
             onClick={toggle}
         >
-            {/* {theme === 'dark' ? <LuSun size={28} /> : <LuMoon size={28} />} */}
-            <LuSun size={28} />
+            {theme === 'dark' ? <LuSun size={28} /> : <LuMoon size={28} />}
         </button>
     );
 }
