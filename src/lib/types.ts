dbabd4be8b2ee4e006682539170f9f16076ff09c
zandonella <123271994+zandonella@users.noreply.github.com>
2026-01-06 23:51:48 -0800

    add supabase test

---
+++ src/lib/types.ts
@@ -0,0 +1,2 @@
+export type Theme = 'light' | 'dark';
+export type ModalMode = 'sign-in' | 'sign-up' | 'forgot-password';
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,17 +1,32 @@
+import AuthModal from '@/components/AuthModal';
+import supabase from '@/lib/supabase.ts';
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+    const [items, setItems] = useState<any>([]);
+
+    useEffect(() => {
+        async function getTodos() {
+            const { data: items } = await supabase.from('test').select();
+
+            if (items && items.length >= 1) {
+                setItems(items);
+            }
+        }
+
+        getTodos();
+    }, []);
+
     return (
         <>
             <div className="text-text bg-bg container mx-auto mt-8 flex w-fit flex-col gap-4 rounded-2xl p-8">
-                <p className="">Welcome to React Router!</p>
-                <button className="bg-bg-dark cursor-pointer rounded-2xl p-2 font-bold transition duration-150 hover:brightness-110">
-                    Click me
-                </button>
-                <button className="bg-primary text-bg-light cursor-pointer rounded-2xl p-2 font-bold transition duration-150 hover:brightness-110">
-                    Click me
-                </button>
-                <button className="bg-accent cursor-pointer rounded-2xl p-2 font-bold transition duration-150 hover:brightness-110">
-                    Click me
-                </button>
+                <p className="">This is the home page.</p>
+                <AuthModal />
+                {items.map((item) => (
+                    <div key={item.id} className="bg-secondary rounded-lg p-4">
+                        <p className="text-text">{JSON.stringify(item)}</p>
+                    </div>
+                ))}
             </div>
         </>
     );
