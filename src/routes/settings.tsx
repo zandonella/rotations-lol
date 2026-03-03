27f7928de2649e11883bfe2102abae814582edb1
zandonella <123271994+zandonella@users.noreply.github.com>
2026-03-03 00:31:47 -0800

    reimplement sign out functionality

--- src/routes/settings.tsx
+++ src/routes/settings.tsx
@@ -1,5 +1,18 @@
 import PasswordResetForm from '@/components/PasswordResetForm';
 import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+import { useAuth } from '@/providers/AuthContext';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 
 export default function Settings() {
     return (
@@ -8,7 +21,51 @@ export default function Settings() {
                 <h1 className="text-2xl font-bold">Settings</h1>
                 <Separator className="my-4 w-full" />
                 <PasswordResetForm />
+
+                <h1 className="mt-4 text-2xl font-bold">Danger Zone</h1>
+                <Separator className="my-4 w-full" />
+                <SignOutDialog />
             </div>
         </>
+    );
+}
+
+function SignOutDialog() {
+    const { signOut } = useAuth();
+
+    function handleSignOut() {
+        signOut();
+    }
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button variant="outline" className="cursor-pointer">
+                    Sign Out
+                </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>
+                        Are you sure you want to sign out?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action will sign you out of your account and you
+                        will need to sign in again to access your account.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel className="cursor-pointer">
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction
+                        onClick={handleSignOut}
+                        variant="destructive"
+                        className="cursor-pointer"
+                    >
+                        Sign Out
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
     );
 }
